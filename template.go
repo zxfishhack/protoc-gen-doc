a7daf63d8c300100bd6c95835e203962a311787e
zxfishhack <zxfishhack@126.com>
2018-07-06 12:10:40 +0800

    1

--- template.go
+++ template.go
@@ -22,7 +22,7 @@ type Template struct {
 }
 
 // NewTemplate creates a Template object from a set of descriptors.
-func NewTemplate(title, stringdescs []*protokit.FileDescriptor) *Template {
+func NewTemplate(title string, stringdescs []*protokit.FileDescriptor) *Template {
 	files := make([]*File, 0, len(descs))
 
 	for _, f := range descs {
