210611aee036f7eb738f271e8be9d25b60ab8e63
zxfishhack <zxfishhack@126.com>
2018-09-20 10:53:07 +0800

    修改md的输出格式

--- filters.go
+++ filters.go
@@ -9,6 +9,7 @@ import (
 
 var (
 	paraPattern         = regexp.MustCompile(`(\n|\r|\r\n)\s*`)
+	paraPattern2        = regexp.MustCompile(`(\n|\r|\r\n)`)
 	spacePattern        = regexp.MustCompile("( )+")
 	multiNewlinePattern = regexp.MustCompile(`(\r\n|\r|\n){2,}`)
 )
@@ -25,10 +26,17 @@ func ParaFilter(content string) string {
 	return fmt.Sprintf("<para>%s</para>", strings.Join(paragraphs, "</para><para>"))
 }
 
-// MDBrFilter splites the content by new lines and postfix each on with \\.
+// MDBrFilter splites the content by new lines and postfix each one with <br/>.
 func MDBrFilter(content string) string {
 	paragraphs := paraPattern.Split(content, -1)
 	return strings.Join(paragraphs, "<br/>")
+}
+
+// MDBlockFilter splites the content by new lines and prefix each one with \t except start with ^
+func MDBlockFilter(content string) string {
+	paragraphs := paraPattern2.Split(content, -1)
+	res := "\t" + strings.Join(paragraphs, "\n\t")
+	return strings.Replace(res, "\t^", "", -1)
 }
 
 // NoBrFilter removes single CR and LF from content.
--- renderer.go
+++ renderer.go
@@ -80,6 +80,7 @@ var funcMap = map[string]interface{}{
 	"para": ParaFilter,
 	"nobr": NoBrFilter,
 	"mdbr": MDBrFilter,
+	"mdbl": MDBlockFilter,
 }
 
 // Processor is an interface that is satisfied by all built-in processors (text, html, and json).
